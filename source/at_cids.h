a966355c5548136d3a503d92b7aa3ce9e5802d7c
Jeff Blake <jtblake@middlebury.edu>
2025-01-18 23:56:24 -0500

    Initial commit

---
+++ source/at_cids.h
@@ -0,0 +1,18 @@
+//------------------------------------------------------------------------
+// Copyright(c) 2025 Jeff.
+//------------------------------------------------------------------------
+
+#pragma once
+
+#include "pluginterfaces/base/funknown.h"
+#include "pluginterfaces/vst/vsttypes.h"
+
+namespace MyCompanyName {
+//------------------------------------------------------------------------
+static const Steinberg::FUID kAbletonTimerProcessorUID (0xCF46E75D, 0xDC565AC7, 0xB796F7CF, 0xAA931D10);
+static const Steinberg::FUID kAbletonTimerControllerUID (0x2C0C2463, 0x94DF57FF, 0x9DC970EE, 0x0E164060);
+
+#define AbletonTimerVST3Category "Fx"
+
+//------------------------------------------------------------------------
+} // namespace MyCompanyName
---
+++ source/at_controller.cpp
@@ -0,0 +1,82 @@
+//------------------------------------------------------------------------
+// Copyright(c) 2025 Jeff.
+//------------------------------------------------------------------------
+
+#include "at_controller.h"
+#include "at_cids.h"
+#include "vstgui/plugin-bindings/vst3editor.h"
+
+using namespace Steinberg;
+
+namespace MyCompanyName {
+
+//------------------------------------------------------------------------
+// AbletonTimerController Implementation
+//------------------------------------------------------------------------
+tresult PLUGIN_API AbletonTimerController::initialize (FUnknown* context)
+{
+	// Here the Plug-in will be instantiated
+
+	//---do not forget to call parent ------
+	tresult result = EditControllerEx1::initialize (context);
+	if (result != kResultOk)
+	{
+		return result;
+	}
+
+	// Here you could register some parameters
+
+	return result;
+}
+
+//------------------------------------------------------------------------
+tresult PLUGIN_API AbletonTimerController::terminate ()
+{
+	// Here the Plug-in will be de-instantiated, last possibility to remove some memory!
+
+	//---do not forget to call parent ------
+	return EditControllerEx1::terminate ();
+}
+
+//------------------------------------------------------------------------
+tresult PLUGIN_API AbletonTimerController::setComponentState (IBStream* state)
+{
+	// Here you get the state of the component (Processor part)
+	if (!state)
+		return kResultFalse;
+
+	return kResultOk;
+}
+
+//------------------------------------------------------------------------
+tresult PLUGIN_API AbletonTimerController::setState (IBStream* state)
+{
+	// Here you get the state of the controller
+
+	return kResultTrue;
+}
+
+//------------------------------------------------------------------------
+tresult PLUGIN_API AbletonTimerController::getState (IBStream* state)
+{
+	// Here you are asked to deliver the state of the controller (if needed)
+	// Note: the real state of your plug-in is saved in the processor
+
+	return kResultTrue;
+}
+
+//------------------------------------------------------------------------
+IPlugView* PLUGIN_API AbletonTimerController::createView (FIDString name)
+{
+	// Here the Host wants to open your editor (if you have one)
+	if (FIDStringsEqual (name, Vst::ViewType::kEditor))
+	{
+		// create your editor here and return a IPlugView ptr of it
+		auto* view = new VSTGUI::VST3Editor (this, "view", "at_editor.uidesc");
+		return view;
+	}
+	return nullptr;
+}
+
+//------------------------------------------------------------------------
+} // namespace MyCompanyName
